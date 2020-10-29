5c13d79029789bcb580165846e8a1c33f4919dcb
Fibonacci <dave.green1664@yahoo.co.uk>
2020-10-29 19:58:10 +0000

    Implemented the functions: is_present() and get(), the test_map now works as expected.

--- maps/CustomMap.h
+++ maps/CustomMap.h
@@ -7,21 +7,28 @@
 
 /** A map from keys of type KeyType to values of type ValueType. */
 template <typename KeyType, typename ValueType>
-class CustomMap {
+class CustomMap
+{
 public:
 	/** Print out the items in the map. */
-	void print() {
+	void print()
+	{
 		std::cout << "Contents:\n";
-		for (const auto& item: items_) {
+
+		for (const auto& item: items_)
+		{
 			std::cout << "- " << item.first;
 			std::cout << " -> " << item.second << "\n";
 		}
 	}
 
 	/** Add or update an entry in the map. */
-	void add(const KeyType& key, const ValueType& value) {
-		for (auto& item: items_) {
-			if (item.first == key) {
+	void add(const KeyType& key, const ValueType& value)
+	{
+		for (auto& item: items_) 
+		{
+			if (item.first == key)
+			{
 				// The key is already in the map.
 				// Update the value for it.
 				item.second = value;
@@ -34,13 +41,32 @@ public:
 	}
 
 	/** Return true if there's a mapping for a key. */
-	bool is_present(const KeyType& key) {
-		std::abort(); // FIXME implement this method
+	bool is_present(const KeyType& key)
+	{
+		for (const auto& item : items_)
+		{
+			if (item.first == key)
+			{
+				return true;
+			}
+		}
+
+		// If we get this far it means we did not find the key.
+		return false;
 	}
 
 	/** Get the value for a key. */
-	const ValueType& get(const KeyType& key) {
-		std::abort(); // FIXME implement this method
+	const ValueType& get(const KeyType& key)
+	{
+		for (const auto& item : items_)
+		{
+			if (item.first == key)
+			{
+				return item.second;
+			}
+		}
+
+		throw std::runtime_error("No key found.");
 	}
 
 private:
--- maps/main.cpp
+++ maps/main.cpp
@@ -17,20 +17,24 @@ using std::to_string;
 // Test that a map class works.
 // (Templated so we can use it with different map classes below.)
 template <typename T>
-void test_map(T& map) {
+void test_map(T& map)
+{
 	cout << "Test adding an item\n";
 	assert(!map.is_present("Bob"));
 	map.add("Bob", 37);
 	assert(map.is_present("Bob"));
 	assert(map.get("Bob") == 37);
+	//assert(map.get("Andy") == 37);
 	map.print();
 
 	cout << "Test adding multiple items\n";
 	map.add("Jim", 42);
 	map.add("Ken", 64);
+	map.add("Andy", 37);
 	assert(map.get("Bob") == 37);
 	assert(map.get("Jim") == 42);
 	assert(map.get("Ken") == 64);
+	assert(map.get("Andy") == 37);
 	map.print();
 
 	cout << "Test updating an item\n";
@@ -42,18 +46,25 @@ void test_map(T& map) {
 
 	const int count = 100;
 	cout << "Test adding and updating " << count << " items\n";
-	for (int i = 0; i < count; i++) {
+
+	for (int i = 0; i < count; i++)
+	{
 		map.add(string("key") + to_string(i), 47);
 	}
-	for (int i = 0; i < count; i++) {
+
+	for (int i = 0; i < count; i++)
+	{
 		map.add(string("key") + to_string(i), i);
 	}
-	for (int i = 0; i < count; i++) {
+
+	for (int i = 0; i < count; i++)
+	{
 		assert(map.get(string("key") + to_string(i)) == i);
 	}
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	cout << "-- Testing CustomMap --\n";
 	CustomMap<string, int> cm_ages;
 	test_map(cm_ages);
